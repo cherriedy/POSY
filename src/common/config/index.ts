9942c3bad8e99e2663b6cc9911561b10f7b2b0ad
cherriedy <huy5adv@gmail.com>
2026-02-13 20:15:23 +0700

    feat: add bulk operation configuration and response DTO; update base repository interface for bulk operations

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,1 +1,2 @@
 export * from './pagination.config';
+export * from './bulk-operation.config';
--- src/common/interfaces/base-repository.interface.ts
+++ src/common/interfaces/base-repository.interface.ts
@@ -2,13 +2,22 @@ import { PaginationParams } from './pagination-params.interface';
 import { Page } from './page.interface';
 
 export abstract class BaseRepository<T extends object = any> {
-  abstract create(entity: T): Promise<T>;
+  abstract create?(entity: T): Promise<T>;
 
-  abstract findById(id: string): Promise<T | null>;
+  abstract bulkCreate?(entities: T[]): Promise<T[]>;
+
+  abstract findById?(id: string): Promise<T | null>;
 
   abstract delete?(id: string): Promise<void>;
 
+  abstract bulkDelete?(ids: string[]): Promise<number>;
+
   abstract update?(id: string, entity: Partial<T>): Promise<T>;
+
+  abstract bulkUpdate?<U extends object = any>(
+    ids: string[],
+    updates: U,
+  ): Promise<number>;
 
   abstract getAll?(params?: PaginationParams): Promise<T[]>;
 
