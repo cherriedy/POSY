be010ee4ca03aef04f944ba9a48c9c8cdf0a9a50
cherriedy <huy5adv@gmail.com>
2026-03-25 22:28:59 +0700

    refactor: remove activity-log module and interceptor

--- src/models/activity-log/interfaces/pagination-params.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ActivityLogFilters } from './index';
-import { PaginationParams } from '../../../common/interfaces';
-
-/**
- * Pagination parameters for activity logs.
- */
-export interface ActivityLogPaginationParams extends PaginationParams {
-  filters?: ActivityLogFilters;
-}
--- src/models/activity-log/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './activity-log.repository-abstract';
-export * from './activity-log.repository-implementation';
-export * from '../interfaces/query-filters.interface';
-export * from '../interfaces/pagination-params.interface';
