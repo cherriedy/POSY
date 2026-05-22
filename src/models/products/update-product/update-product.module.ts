687bd3753023920d9731bcd75b305ce17f452360
cherriedy <huy5adv@gmail.com>
2026-05-22 19:13:17 +0700

    feat(refactor): S1 - critical bug fixes C1-C4 + N5

--- src/models/products/update-product/update-product.module.ts
+++ src/models/products/update-product/update-product.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UpdateProductService } from './update-product.service';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [
-    EventEmitterModule.forRoot({
-      wildcard: true,
-      delimiter: '.',
-    }),
-  ],
   providers: [UpdateProductService],
   exports: [UpdateProductService],
 })
